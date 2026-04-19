5d16adfe014194a7385f1feb66a6c87144921922
Arthur Peruchi <arthurgptrindade@gmail.com>
2026-04-19 20:15:08 -0300

    Fix: Rodapé agora está devidamente organizado na seção de componentes, pois é algo reutilizável nas demais páginas

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import Sobre from './sections/Sobre/Sobre'
 import Instituicoes from './sections/Instituicoes/Instituicoes'
 import ProvaSocial from './sections/ProvaSocial/ProvaSocial'
 import CTAFinal from './sections/CTAFinal/CTAFinal'
-import Rodape from './sections/Rodape/Rodape'
+import Rodape from '../../components/Rodape/Rodape'
 
 export default function Home() {
     return (
