b69559397896d383693c79d49a7e00c05131d637
Arthur Peruchi <arthurgptrindade@gmail.com>
2026-09-07 16:28:18 -0300

    feat: estruturação do modelo de doacoes e necessidades com base no banco

--- frontend/src/types/domain.ts
+++ frontend/src/types/domain.ts
@@ -11,15 +11,81 @@ export interface Instituicao {
     id: number;
     nome: string;
     logo?: string;
+    cnpj?: string;
     cidade: string;
     uf: string;
-    cnpj?: string;
+    endereco?: string;
     animaisSobCuidado?: number;
     horarioFuncionamento?: string;
     descricao?: string;
-    principaisNecessidades?: string;
     aceitaVoluntarios?: boolean;
-    endereco?: string;
-    redesSociais?: RedesSociais;
     aceitaDoacoes: boolean;
+    redesSociais?: RedesSociais;
+    necessidades?: Necessidade[];
 }
+
+export interface Necessidade {
+    id: number;
+    nome: string;
+    descricao?: string;
+    prioridade: PrioridadeNecessidade;
+    quantidadeMeta?: number;
+    unidade?: UnidadeMedida;
+}
+
+export interface Doacao {
+    id: number;
+    idUsuario: number;
+    idInstituicao: number;
+    tipoDoacao: TipoDoacao;
+    status: StatusDoacao;
+    feedback?: string;
+}
+
+export interface ItemDoacaoProduto {
+    id: number;
+    nome: string;
+    descricao?: string;
+    // marca?: string;
+    // especie?: "cachorro" | "gato" | "outro";
+    // faseVida?: "filhote" | "adulto" | "idoso" | "todas";
+    // peso?: number;
+    quantidade: number;
+    unidade?: UnidadeMedida;
+}
+
+export interface Visita {
+    id: number;
+    idDoacao: number;
+    data: string;
+    horario: string;
+}
+
+export interface DoacaoMonetaria extends Doacao {
+    tipoDoacao: "monetaria";
+    valor: number;
+    formaPagamento: FormaPagamento;
+    recorrente: boolean;
+}
+
+export interface DoacaoProdutoBase extends Doacao {
+    tipoDoacao: "produto";
+    itens: ItemDoacaoProduto[];
+}
+
+export interface DoacaoProdutoVisita extends DoacaoProdutoBase {
+    formaEntrega: "visita";
+    visita: Visita;
+}
+
+export interface DoacaoProdutoParceiro extends DoacaoProdutoBase {
+    formaEntrega: "compra_parceiro";
+}
+
+export type TipoUsuario = "administrador" | "instituicao" | "doador";
+export type TipoDoacao = "monetaria" | "produto";
+export type StatusDoacao = "pendente" | "confirmada" | "cancelada";
+export type PrioridadeNecessidade = "alta" | "media" | "baixa";
+export type UnidadeMedida = "unidade" | "kg" | "g" | "l" | "ml" | "caixa" | "pacote" | "outro";
+export type FormaPagamento = "pix" | "cartao_credito"
+export type FormaEntrega = "visita" | "compra_parceiro";
