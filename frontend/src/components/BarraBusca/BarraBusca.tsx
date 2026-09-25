df662737bf1a758db7364bfe50266df45a8d4878
Arthur Peruchi <arthurgptrindade@gmail.com>
2026-09-25 17:30:12 -0300

    feat: melhora UI da barra de busca e rodapé em dispositivos mobile

--- frontend/src/components/BarraBusca/BarraBusca.tsx
+++ frontend/src/components/BarraBusca/BarraBusca.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import "./BarraBusca.css";
 import Botao from "../Botao/Botao";
+import useIsMobile from '../../hooks/useIsMobile';
 
 type BuscaProps = {
   placeholder?: string;
@@ -19,6 +20,8 @@ export default function BarraBusca({
     value,
     onChange
 }: BuscaProps) {
+    const isMobile = useIsMobile();
+
     return (
         <div className="barra-busca" style={style}>
             <div className="icone-busca">
@@ -29,18 +32,28 @@ export default function BarraBusca({
                 type={type} 
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
-                placeholder={placeholder || "Buscar..."} 
+                placeholder={isMobile ? "Buscar..." : placeholder} 
                 id="input-busca" 
             />
             
-            {/* Funcionalidade pendente */}
+            {isMobile && (
+                <div className="mobile-filtro">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#f5f5f5" viewBox="0 0 256 256">
+                        <path d="M227.81,66.76l-.08.09L160,139.17v55.49A16,16,0,0,1,152.87,208l-32,21.34A16,16,0,0,1,96,216V139.17L28.27,66.85l-.08-.09A16,16,0,0,1,40,40H216a16,16,0,0,1,11.84,26.76Z"></path>
+                    </svg>
+                </div>
+            )}
+
+            {!isMobile && (
             <Botao 
                 size="md"
                 fullWidth
                 style={{ height: "100%", padding: "0 40px", borderRadius: "0 0.5rem 0.5rem 0" }}
+                className="btn-adicionar-filtro"
             >
                 Adicionar Filtro
             </Botao>
+            )}
         </div>
     );
 }
